6c7f3672ad4c14fc89f564cda6a6e62b7cdb6aaa
Eric Hare <ericrhare@gmail.com>
2015-03-06 07:14:31 -0600

    Allow building and downloading of logo plots

--- app/server.R
+++ app/server.R
@@ -15,28 +15,45 @@ function(input, output) {
         values$seqs <<- list(peptide = strsplit(input$sequence, split = ",")[[1]])
     })
     
-    output$logoplot <- renderPlot({
+    output$plotbuilt <- reactive({
+        return(length(values$seqs) > 0)
+    })
+    outputOptions(output, 'plotbuilt', suspendWhenHidden=FALSE)
+    
+    myplot <- reactive({
         if (length(values$seqs) == 0) return(NULL)
         
-        dm2 <- splitSequence(as.data.frame(values$seqs), "peptide")
-        cols <- c("grey80", brewer.pal(3,"RdYlBu"))
-        
-        dm3 <- calcInformation(dm2, pos="position", elems="element", k=21)
-        dm3b <- merge(dm3, aacids, by.x="element", by.y="AA", all.x=T)
-        
-        print(
+        withProgress(message = "Building logo plot, please wait...", expr = {
+            dm2 <- splitSequence(as.data.frame(values$seqs), "peptide")
+            cols <- c("grey80", brewer.pal(3,"RdYlBu"))
+            
+            dm3 <- calcInformation(dm2, pos="position", elems="element", k=21)
+            dm3b <- merge(dm3, aacids, by.x="element", by.y="AA", all.x=T)
+            
             ggplot(dm3b, aes(x=position, y=bits, group=element, label=element, fill=Polarity), alpha=0.8) + 
                 geom_hline(yintercept=-log(1/21, base=2), colour="grey30", size=0.5) + 
                 geom_logo() + 
                 scale_fill_manual("Polarity", values=cols) +  
                 theme_bw() + theme(legend.position="bottom") + 
-                ylab("bits") + 
+                xlab(input$xlab) +
+                ylab(input$ylab) + 
+                ggtitle(input$title) +
                 theme(plot.margin=unit(c(0,0,0,0), "cm")) + 
                 geom_hline(yintercept=0, colour="white", size=0.5) + 
                 geom_hline(yintercept=0, colour="grey30", size=0.125) + 
                 scale_y_continuous(breaks=c(-1,0,1,2,4), labels=c(1,0,1,2,4))
-        )
-        
+        })
     })
+    
+    output$logoplot <- renderPlot({
+        print(myplot())
+    })
+    
+    output$download <- downloadHandler(
+        filename = function() { paste("logoplot", input$image_format, sep = ".") },
+        content = function(file) {
+            ggsave(file, plot = myplot(), dpi = input$dpi)
+        }
+    )
     
 }
--- app/ui.R
+++ app/ui.R
@@ -1,21 +1,45 @@
 library(shiny)
+library(shinythemes)
 
 ## Legend with letters, color, name
 ## Choice of color scheme ala weblogo
 
-fluidPage(
+fluidPage(theme = shinytheme("united"),
     
     titlePanel("GGLogo Prototype"),
     
     sidebarLayout(
         sidebarPanel(
+            h4("Sequence"),
+            
             helpText("Input a list of sequences, separated by commas"),
             
             tags$textarea(id="sequence", rows=3, cols=40, "RWTHLASGRT,RWLSLBSGRT,RWTHLSSGRT"),
+
+            actionButton("confirm", "Build Logo Plot"),
+            
+            conditionalPanel(condition = "output.plotbuilt == true",
+                downloadButton("download", "Download Logo Plot")
+            ),
             
             hr(),
             
-            actionButton("confirm", "Build Logo")
+            checkboxInput("advanced", "Show Advanced Configuration"),
+            
+            conditionalPanel(condition = "input.advanced == true",
+                h4("Advanced Configuration"),
+                                
+                h5("Plot Labels"),
+                textInput("title", "Plot Title", value = ""),
+                textInput("xlab", "X Axis Label", value = "position"),
+                textInput("ylab", "Y Axis Label", value = "bits"),
+                
+                hr(),
+                
+                h5("Download Options"),
+                selectizeInput("image_format", "Image Format", choices = c("PNG" = "png", "JPG" = "jpg", "PDF" = "pdf")),
+                numericInput("dpi", "DPI", value = 300, min = 1, max = 1000)
+            )
         ),
         
         mainPanel(
